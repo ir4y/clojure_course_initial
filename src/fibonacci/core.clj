b97b0620faf151be03ee84ac9c57e079403f154f
Ilya Beda <ir4y.ix@gmail.com>
2013-03-09 23:28:37 +0800

    Task1 reduce optimization

--- src/fibonacci/core.clj
+++ src/fibonacci/core.clj
@@ -1,6 +1,26 @@
 (ns fibonacci.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn even-sum [& args]
+   (reduce + (map (fn [x]
+                    (if (= (mod x 2) 0)
+                    x
+                    0)) 
+            args))
+  )
+
+
+(defn fib [max]
+  (defn _fib 
+    ([] (_fib 1 1 ))
+    ([x y]
+      (if (> y max) 
+        [y] 
+        (concat [x]  (_fib y (+ x y))))))
+  _fib)
+
+
+
+(defn -main
+    [& args]
+    (println  (apply even-sum ((fib 4000000)) )))
+
